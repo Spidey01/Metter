7f9feaf7424be39cfb6761583c54662265eddb8c
Terry M. Poulin <BigBoss1964@gmail.com>
2012-08-04 22:30:20 +0000

    Stub a simple list activity

---
+++ android/src/main/java/com/spidey01/metter/android/NoteListActivity.java
@@ -0,0 +1,51 @@
+package com.spidey01.metter.android;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class NoteListActivity extends ListActivity {
+
+    /** The activity is being created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.note_list);
+    }
+
+    /* The activity is about to become visible.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    /* The activity has become visible (it is now "resumed").
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /* Another activity is taking focus (this activity is about to be "paused").
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /* The activity is no longer visible (it is now "stopped")
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    /* The activity is about to be destroyed.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
