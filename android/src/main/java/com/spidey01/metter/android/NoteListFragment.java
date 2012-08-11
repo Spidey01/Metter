a420709c9041cd37079e1b112029ae181fe01113
Terry M. Poulin <BigBoss1964@gmail.com>
2012-08-11 21:06:46 +0000

    Finally commit test data.

--- android/src/main/java/com/spidey01/metter/android/NoteListFragment.java
+++ android/src/main/java/com/spidey01/metter/android/NoteListFragment.java
@@ -13,11 +13,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
 public class NoteListFragment extends ListFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String[] values = new String[] {
+            "Note 1", "Note 2", "Note 3", "Note 4", "Note 5", "Note 6",
+            "Note 7", "Note 8", "Note 9", "Note 10", "Note 11", "Note 12",
+        };
+
+        // First paramenter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                values);
+        setListAdapter(adapter);
     }
 
     @Override
